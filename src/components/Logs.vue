18760bbbe04d2e5d12e75aa84bce5cf175cc73d9
Ryan <ryan.dsilva.98@gmail.com>
2018-10-22 23:09:44 +0530

    Added Container

--- src/components/Logs.vue
+++ src/components/Logs.vue
@@ -1,10 +1,16 @@
 <template>
   <div id="logs" class="text-xs-center">
-    <v-data-table :headers="headers" :items="getLogs" hide-actions class="elevation-1">
-      <template slot="items" slot-scope="props">
-        <td class="text-xs-center">{{ new Date(props.item.time).toLocaleString() }}</td>
-      </template>
-    </v-data-table>
+    <v-container>
+      <v-layout row wrap>
+        <v-flex xs12>
+          <v-data-table :headers="headers" :items="getLogs" hide-actions class="elevation-1">
+            <template slot="items" slot-scope="props">
+              <td class="text-xs-center">{{ new Date(props.item.time).toLocaleString() }}</td>
+            </template>
+          </v-data-table>
+        </v-flex>
+      </v-layout>
+    </v-container>
   </div>
 </template>
 
