4159e5513bff2f6f3dc51a74695a26f4d5fdd0a5
Ryan <ryan.dsilva.98@gmail.com>
2018-10-23 09:20:05 +0530

    Cleanup

--- src/main.js
+++ src/main.js
@@ -1,13 +1,11 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
-import VueFire from 'vuefire'
 import 'vuetify/dist/vuetify.min.css'
 import App from './App'
 import router from './router'
 
 Vue.config.productionTip = false
 Vue.use(Vuetify)
-Vue.use(VueFire)
 
 /* eslint-disable no-new */
 new Vue({
